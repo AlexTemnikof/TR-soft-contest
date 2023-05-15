299a40b307f1a04375180e2b19e923eea8875d61
Alexei <alexei.temnikov02@gmail.com>
2023-05-15 18:49:12 +0300

    feat: implemented authorization

--- src/main/java/com/example/contestapp/auth/utils/JwtUtils.java
+++ src/main/java/com/example/contestapp/auth/utils/JwtUtils.java
@@ -7,9 +7,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class JwtUtils {
